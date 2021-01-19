699095948e3ecd1875f014e91225ae987e726dce
Petro <kolosovp94@gmail.com>
2021-01-20 00:08:22 +0100

    rotor set right rotation implemented

--- Enigma/Implementations/RotorSet.cs
+++ Enigma/Implementations/RotorSet.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Enigma.Interfaces;
 
@@ -6,9 +7,7 @@ namespace Enigma.Implementations
 {
     public class RotorSet : IRotorSet
     {
-        public List<IRotor> Rotors { get; }
-
-        private int[] _positions;
+        public List<IRotor> Rotors { get; private set; }
 
         public RotorSet(int rotorsNumber)
         {
@@ -18,38 +17,48 @@ namespace Enigma.Implementations
 
         public RotorSet(int[] positions)
         {
-            _positions = positions;
             Rotors = new List<IRotor>(positions.Select(x => new Rotor(x)));
         }
 
         public int[] GetPositions()
         {
-            throw new System.NotImplementedException();
+            return Rotors.Select(x => x.Position).ToArray();
         }
 
         public void SetPositions(int[] positions)
         {
-            throw new System.NotImplementedException();
+            if (positions.Length != Rotors.Count)
+                throw new InvalidOperationException("Rotor count should be equal to pos. count.");
+
+            Rotors = new List<IRotor>(positions.Select(x => new Rotor(x)));
         }
 
         public int GetEncryptedIndexAndRotate(int index)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public int GetDecryptedIndexAndRotate(int index)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public void RightRotate(int shift)
         {
-            throw new System.NotImplementedException();
+            var rotor = Rotors[0];
+            rotor.RightRotate(shift);
+
+            for (var i = 1; i < Rotors.Count; i++)
+            {
+                var difference = Rotors[i - 1].TotalRotationsCount - Rotors[i].Position;
+                if (difference > 0) 
+                    Rotors[i].RightRotate(difference);
+            }
         }
 
         public void LeftRotate(int shift)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
